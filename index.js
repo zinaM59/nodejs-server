e08675cca170820a51605a0e36b6f6c933e8751d
zinaM59 <61130578+zinaM59@users.noreply.github.com>
2024-08-20 01:14:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require ( 'express' )
 const app = express () 
 const port = 3000
 
- app. get ( '/', ( req, res ) => { 
-  res. send ( 'Hello World!' ) 
+ app.get ( '/', ( req, res ) => { 
+  res.send ( 'Hello World!' ) 
 }) 
 
-app. Listen (port, () => { 
-  console . log ( `Пример приложения, прослушивающего порт ${port} ` ) 
+app.Listen (port, () => { 
+  console.log ( `Пример приложения, прослушивающего порт ${port} ` ) 
 })
